3261abc7e57b0626d9daccd22332055afe3a8e26
Samuel Coleman <samuel.e.c519@gmail.com>
2026-06-22 13:45:11 -0600

    Update composer files to include ext-pdo_mysql and adjust error reporting settings in index.php

--- public/index.php
+++ public/index.php
@@ -10,6 +10,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
+if (($_ENV['APP_ENV'] ?? 'local') === 'production') {
+    ini_set('display_errors', '0');
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', '1');
+    error_reporting(E_ALL);
+}
+
 session_start();
 
 $router = new Router();
