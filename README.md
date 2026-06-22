33bda154a2f2c4f4036e37d79d7013fc1fefb8f3
Samuel Coleman <samuel.e.c519@gmail.com>
2026-06-21 22:52:44 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pollify PHP
+
+A vanilla PHP polling application with users, polls, and ballots.
+
+## Local development
+
+Start the PHP development server:
+
+```bash
+php -S localhost:8000 -t public
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$appName = 'Pollify PHP';
+
+echo "<h1>{$appName}</h1>";
+echo '<p>Your vanilla PHP polling app is running.</p>';
